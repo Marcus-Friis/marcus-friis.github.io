37ceadd1facb5d37418b84ea7d3613a480a1ec1d
Marcus Friis <marcus.friis99@gmail.com>
2026-05-06 22:39:12 +0200

    Updates

---
+++ _app/immutable/chunks/L1X2eSh_.js
@@ -0,0 +1,1 @@
+const t="has-visited",s=()=>localStorage.getItem(t)===null,e=()=>{localStorage.setItem(t,"true")};export{s as i,e as m};
---
+++ _app/immutable/chunks/drP9CS2z.js
@@ -0,0 +1,1 @@
+import{_ as o,Y as t,as as c,$ as l}from"./D7zdPJYv.js";function u(e){throw new Error("https://svelte.dev/e/lifecycle_outside_component")}function i(e){t===null&&u(),c&&t.l!==null?a(t).m.push(e):o(()=>{const n=l(e);if(typeof n=="function")return n})}function a(e){var n=e.l;return n.u??={a:[],b:[],m:[]}}export{i as o};
